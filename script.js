452416548b1b037ad702ac8277adf5f6cb165b4c
lynaxho-89 <125113735+lynaxho-89@users.noreply.github.com>
2023-04-16 13:11:20 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if(this.scrollY > 20){
+      $('.navbar').addClass("sticky");
+    } else {
+      $('.navbar').removeClass("sticky")
+    }
+    if(this.scrollY > 500){
+      $('.scroll-up-btn').addClass("show")
+    } else {
+      $('.scroll-up-btn').removeClass("show")
+    }
+  });
+  var typed = new Typed(".typing",{
+    strings: ["Web Designer", "Bot Developer", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+  $('.scroll-up-btn').click(function(){
+    $('html').animate({scrollTop: 0})
+  });
+  $('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active")
+  });
+});
